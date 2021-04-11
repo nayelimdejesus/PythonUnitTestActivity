b4eeb2f6565dd9ff7eb663a59d4660ba7c951e96
Nayeli De Jesus <nayelidjm142@gmail.com>
2021-04-11 06:48:53 +0000

    added a test function that tests if username has special characters

--- tests/test_validator.py
+++ tests/test_validator.py
@@ -47,3 +47,13 @@ class TestValidator(unittest.TestCase):
 
         # Assert
         self.assertTrue(result)
+        
+    def test_it_will_reject_username_if_contains_special_characters(self):
+        # Assume
+        username = 'Rev@l$'
+        
+        # Action
+        result = self.validator.username_is_valid(username)
+        
+        # Assert
+        self.assertFalse(result)
